507eac48ab3ba7bf5fd2e9636bc444910eccb85e
Matheus Ferreira <matheus.fer33@gmail.com>
2023-03-07 20:30:06 -0300

    login functionality implemented

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -21,3 +21,25 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+
+// login the user
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await UserModel.findOne({ username });
+        if (!user) {
+            return res.status(400).json({ message: 'User does not exist' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h'
+        });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
