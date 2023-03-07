c9162cb2881e4a829a16bd49055cbc33c58a15bc
Matheus Ferreira <matheus.fer33@gmail.com>
2023-03-07 20:42:15 -0300

    frontend structure and navbar done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import SavedRecipes from "./pages/SavedRecipes";
+import CreateRecipe from "./pages/CreateRecipe";
+import Navbar from "./components/Navbar";
+
+
 const App = () => {
-  return <h1>teste</h1>;
+  return <>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/savedrecipes" element={<SavedRecipes />} />
+      <Route path="/createrecipe" element={<CreateRecipe />} />
+    </Routes>
+  </Router>
+  </>
 };
 
 export default App;
