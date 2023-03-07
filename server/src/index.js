ce63ce1ba26c3f896722acea3b106ca330551de1
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-03-07 15:08:16 -0300

    creation of mongoose models and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,26 @@
 import express from "express";
 import cors from "cors";
-// import dotenv from "dotenv";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import userRouter from "./routes/users.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
